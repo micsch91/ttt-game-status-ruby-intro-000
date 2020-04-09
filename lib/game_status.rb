87a055cbbb69d2a88aed33eb158aa74f083d2d80
Michael Schneider <mic.sch91@gmail.com>
2020-04-09 13:53:05 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,64 +16,20 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
- WIN_COMBINATIONS.detect do |win_combination|
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
 
- win_index_1 = win_combination[0]
- win_index_2 = win_combination[1]
- win_index_3 = win_combination[2]
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
 
-  position_1 = board[win_index_1]
-  position_2 = board[win_index_2]
-  position_3 = board[win_index_3]
-
-    if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
-      return win_combination
-    else
-      return false
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
     end
- end
-  WIN_COMBINATIONS.detect do |combo|
-    board[combo[0]] == board[combo[1]] &&
-    board[combo[1]] == board[combo[2]] &&
-    position_taken?(board, combo[0])
-  end
+  }
+  return false
 end
-
-def full?(board)
-  board.all? { |index| index == "X" || index == "0" }
-end
-
-def draw?(board)
-  if !won?(board) && full?(board)
-    return true
-  elsif !won?(board) && !full?(board)
-    return false
-  else won?(board)
-    return false
-  end
-end
-
-def over?(board)
-  if won?(board) || full?(board) || draw?(board)
-    return true
-  else
-    return false
-  end
-end
-
-def winner(board)
-  if won?(board)
-    return board[won?(board)[0]]
- index = []
- index = won?(board)
- if index == false
-   return nil
- else
-   if board[index[0]] == "X"
-     return "X"
-   else
-     return "O"
-end
-  end
-    end
-      end
